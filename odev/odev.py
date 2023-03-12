d53503e8205a7a356d342fa8d87d07cd6b2a576c
oguzhanbykl <125604710+oguzhanbykl@users.noreply.github.com>
2023-03-12 21:25:57 +0300

    Add files via upload

---
+++ odev/odev.py
@@ -0,0 +1,22 @@
+# *******Python'da Veri Tiplerini araştırınız, her bir veri tipi için kendi cümlelerinizle açıklamalar yazınız.*******
+
+=> string: Yazı tipidir.String veri tipini kullanmak istediğimizde "" ya da '' içinde kullanmalıyız.
+=> integer: Tam sayı değerleridir. (örn: 5,25,36,1203 vb.)
+=> float: Ondalıklı sayı değeridir. (örn: 2.3,0.6,25.9,50.0 vb.)
+=> boolian: Bir değerin "True" ya da "False" olduğunu belirtir. 
+
+# ********Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.*******
+
+- Arama kısmı : int ya da str
+
+- Kurs ilerleme : int (eğer %55.4 gibi bir değer çıkıyorsa float)
+
+# Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.
+
+kullaniciAdi = oguzhanbykl
+
+if (kullaniciadi==oguzhanbykl):
+    print("Kullanıcı Başarıyla Giriş Yaptı.")
+else:
+    print("Kullanıcı Adı Yanlış.")
+    
